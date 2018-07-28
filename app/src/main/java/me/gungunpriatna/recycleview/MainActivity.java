c4d9441da72f502404009cccef82379de71ef17b
Gun Gun Priatna <gungun002@ummi.ac.id>
2018-07-28 07:51:07 +0700

    menambahkan data, model adapter

--- app/src/main/java/me/gungunpriatna/recycleview/MainActivity.java
+++ app/src/main/java/me/gungunpriatna/recycleview/MainActivity.java
@@ -2,12 +2,36 @@ package me.gungunpriatna.recycleview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import me.gungunpriatna.recycleview.model.President;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView rvCategory;
+    private ArrayList<President>list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rvCategory = findViewById(R.id.rv_category);
+
+        rvCategory.setHasFixedSize(true);
+
+        list = new ArrayList<>();
+        list.addAll(PresidentData.getListData());
+
+        showRecyclerList();
+    }
+
+    private void showRecyclerList() {
+        rvCategory.setLayoutManager(new LinearLayoutManager(this));
+
+        ListPresidentAdapter listPresidentAdapter = new ListPresidentAdapter(this);
+        listPresidentAdapter.setListPresident(list);
+        rvCategory.setAdapter(listPresidentAdapter);
     }
 }
